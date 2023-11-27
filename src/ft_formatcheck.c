cc342ba0a55111d6053b423952442f5e1e58606d
Nigel Robinson <nrobinso@made-f0Cr6s2.clusters.42paris.fr>
2023-11-27 18:22:58 +0100

    ft_formatcheck

--- src/ft_formatcheck.c
+++ src/ft_formatcheck.c
@@ -6,8 +6,17 @@
 /*   By: nrobinso <nrobinso@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/27 18:13:47 by nrobinso          #+#    #+#             */
-/*   Updated: 2023/11/27 18:17:48 by nrobinso         ###   ########.fr       */
+/*   Updated: 2023/11/27 18:22:34 by nrobinso         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-size_t ft_formatcheck(char *str, )
+size_t ft_formatcheck(char *str, va_list input)
+{
+	size_t count;
+
+	count = 0;
+	
+	putchar(va_arg(input, int) + '0');
+
+	return (count);
+}
