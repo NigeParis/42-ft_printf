ceda94e6ea4734c469d74d20ccad20db4970e8c2
Nigel Robinson <nrobinso@made-f0Cr6s2.clusters.42paris.fr>
2023-11-27 18:18:00 +0100

    ft_formatcheck

--- src/ft_formatcheck.c
+++ src/ft_formatcheck.c
@@ -6,8 +6,8 @@
 /*   By: nrobinso <nrobinso@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/27 18:13:47 by nrobinso          #+#    #+#             */
-/*   Updated: 2023/11/27 18:13:49 by nrobinso         ###   ########.fr       */
+/*   Updated: 2023/11/27 18:17:48 by nrobinso         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-
+size_t ft_formatcheck(char *str, )
